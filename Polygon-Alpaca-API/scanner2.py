3fc6504c5caf609c3822f5624f00a7907d4ba064
wildape1 <55204681+wildape1@users.noreply.github.com>
2020-10-21 20:50:17 -0700

    crossreference stock data from polygon with nasdaq

--- Polygon-Alpaca-API/scanner2.py
+++ Polygon-Alpaca-API/scanner2.py
@@ -20,29 +20,46 @@ def main():
  
     #further filtering
     nasdaq_tradedQ = nasdaq_traded[nasdaq_traded["Market Category"] == "Q"]
-    #print(nasdaq_tradedQ)
+    print(nasdaq_tradedQ)
 
     key = config.API_KEY
     polygon_snapshot = pd.DataFrame(columns=['Symbol', 'todaysChange'])
+
+    print(nasdaq_tradedQ.Symbol)
+    
     
     with RESTClient(key) as client:
         response = client.stocks_equities_snapshot_all_tickers()
         print("Reading data from nasdaq list")
+        tickernum = 0
         for ticker in response.tickers:
+            tickernum += 1
+            if tickernum % 100 == 0:
+                print(str(tickernum) + " tickers read")
             #print(f" {ticker['ticker']} dropped {ticker['todaysChange']}$ which was {ticker['todaysChangePerc']}% updated @ {ticker['updated']}")
-            new_row = {'Symbol':ticker['ticker'], 'todaysChange':ticker['todaysChange']}
             #print(new_row)
-            polygon_snapshot = polygon_snapshot.append(new_row, ignore_index=True)
+            symbol = ticker['ticker']
+            if symbol in nasdaq_tradedQ.values:
+                #print(str(symbol) + " is in nasdaq_tradedQ")
+                new_row = {'Symbol':ticker['ticker'], 'todaysChange':ticker['todaysChange']}
+                polygon_snapshot = polygon_snapshot.append(new_row, ignore_index=True)
 
+    
     print(polygon_snapshot)
-        
+    #tickers = ['AAPL', 'MSFT']
+    #polygon_snapshot = polygon_snapshot[polygon_snapshot.Symbol.isin(nasdaq_tradedQ.Symbol)]
+    #print(polygon_snapshot)
+    #polygon_snapshot = polygon_snapshot[polygon_snapshot.Symbol.isin(tickers)]
+    #print(polygon_snapshot)
 
 
 
 if __name__ == '__main__':
+
+    main()
     while(1):
         now = datetime.now()
-        if now.hour == 10 and now.minute == 50:
+        if now.hour == 6 and now.minute == 30:
             main()
             quit()
     
