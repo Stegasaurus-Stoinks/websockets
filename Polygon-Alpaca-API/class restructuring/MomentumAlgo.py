400b099845b7019671b99ec839c4c1966c10a89c
justinvred <jvredevoogd@cfms4.com>
2021-02-18 18:17:52 -0700

    More progress

--- Polygon-Alpaca-API/class restructuring/MomentumAlgo.py
+++ Polygon-Alpaca-API/class restructuring/MomentumAlgo.py
@@ -2,6 +2,8 @@
 from trade import Trade
 from datetime import datetime
 from algo import Algo
+import keystore
+
 #outside imports
 import select, time, requests, json, sys
 import psycopg2
@@ -9,8 +11,7 @@ import psycopg2.extensions
 import config
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
-import alpaca_trade_api as tradeapi
-import keystore
+import alpaca_trade_api as alpacaApi
 import numpy as np
 import pytz
 
@@ -48,8 +49,8 @@ class MomentumAlgo(Algo):
         self.profit = 0
 
         #----------------------------
-        self.utc=pytz.UTC
-        self.Start_time = utc.localize(Start_time) 
+        utc=pytz.UTC
+        self.Start_time = utc.localize(self.Start_time) 
         self.NewData = False
         self.AM_candlesticks = []
         self.close_data = []
@@ -57,72 +58,74 @@ class MomentumAlgo(Algo):
         self.Next_time = datetime(2020, 11, 18, 18, 11, 0)
         self.in_position = False
 
-        self.DAY_START_TIME = Start_time.replace(hour=14, minute=30)
-        self.DAY_END_TIME = Start_time.replace(hour=21, minute=00)
+        self.DAY_START_TIME = self.Start_time.replace(hour=14, minute=30)
+        self.DAY_END_TIME = self.Start_time.replace(hour=21, minute=00)
 
 
     def update(self):
         if(self.Live_Trading):
-            AwaitNewData() 
+
+            AwaitNewData() #Is this method now supposed to be algo.update? or will we be checking for new data in the main?***************
+
         #Breaks out to the code below if a notify is recieved on the above defined "notify_channel"
         #------------Add code Below Here----------
         if(not self.Live_Trading):
             try:
                 data = QuerySpecificData(self.ticker, self.Current_time)
-                UpdateDataArray(data)
+                self.UpdateDataArray(data)
                 #O = AM_candlesticks[-1]['open']
 
             except (IndexError):
                 print("missing data")
                 self.missing_data += 1
-                Current_time = Current_time + timedelta(minutes=1)
+                self.Current_time = self.Current_time + timedelta(minutes=1)
 
 
         if(self.Live_Trading):
-            UpdateDataArray(QueryData(self.ticker))
-            Current_time = self.AM_candlesticks[-1]['dtime']
+            self.UpdateDataArray(QueryData(self.ticker))
+            self.Current_time = self.AM_candlesticks[-1]['dtime']
 
         #print(Current_time)
         #print(DAY_END_TIME)
         #print(DAY_START_TIME)
-        if(ValidTradingHours(Current_time)):
+        if(ValidTradingHours(self.Current_time)):
 
             C = self.AM_candlesticks[-1]['close']
 
-            if EMA1 == 0:
-                EMA1 = C
-                EMA2 = C
+            if self.EMA1 == 0:
+                self.EMA1 = C
+                self.EMA2 = C
 
-            EMA1 = Calc_EMA(20,C,EMA1)
-            EMA2 = Calc_EMA(50,C,EMA2)
+            self.EMA1 = Calc_EMA(20,C,self.EMA1)
+            self.EMA2 = Calc_EMA(50,C,self.EMA2)
             #print(O, C)
 
             #getting first EMA point :D
             if len(self.AM_candlesticks) == 2:
-                if EMA1 > EMA2:
-                    highest = 1
+                if self.EMA1 > self.EMA2:
+                    self.highest = 1
                 else:
-                    highest = 2
+                    self.highest = 2
 
-            print(highest)
+            print(self.highest)
 
             if self.in_position:
                 if not self.upTrade:
                     #calculate trailing stop loss
                     temp = C + (C * self.stop_loss)
-                    if temp < exit_price:
-                        exit_price = temp
+                    if temp < self.exit_price:
+                        self.exit_price = temp
                     
                     #if C > exit_price:
-                    if EMA1 > EMA2 or not ValidTradingHours(Current_time, -3):
-                        in_position = False
-                        Sell_Position()
+                    if self.EMA1 > self.EMA2 or not ValidTradingHours(self.Current_time, -3):
+                        self.in_position = False
+                        Sell_Position()#Will be done through trade/position class*************
                         self.number_of_trades += 1
-                        profit = profit + (self.entry_price - self.exit_price)
-                        print("Exited Trade at " + str(exit_price))
-                        print("Trade profit: " + str(self.entry_price - exit_price))
-                        print("Total Profit: " + str(profit))
-                        if entry_price - exit_price > 0:
+                        self.profit = self.profit + (self.entry_price - self.exit_price)
+                        print("Exited Trade at " + str(self.exit_price))
+                        print("Trade profit: " + str(self.entry_price - self.exit_price))
+                        print("Total Profit: " + str(self.profit))
+                        if self.entry_price - self.exit_price > 0:
                             self.good_trades += 1
 
                         else:
@@ -135,58 +138,85 @@ class MomentumAlgo(Algo):
                         exit_price = temp
 
                     #if C < exit_price:
-                    if EMA2 > EMA1 or not ValidTradingHours(Current_time, -5):
-                        in_position = False
-                        Sell_Position()
-                        number_of_trades += 1
-                        profit = profit + (self.exit_price - self.entry_price)
+                    if self.EMA2 > self.EMA1 or not ValidTradingHours(self.Current_time, -5):
+                        self.in_position = False
+                        Sell_Position()#Will be done through trade/position class*********
+                        self.number_of_trades += 1
+                        self.profit = self.profit + (self.exit_price - self.entry_price)
                         print("Trade profit: " + str(self.exit_price - self.entry_price))
-                        print("Total Profit: " + str(profit))
-                        if exit_price - entry_price > 0:
+                        print("Total Profit: " + str(self.profit))
+                        if self.exit_price - self.entry_price > 0:
                             self.good_trades += 1
 
                         else:
                             self.bad_trades += 1
 
             else:
-                if len(api.list_positions()) == 0:
+                if len(api.list_positions()) == 0:  #list_positions can be switched to a value connecting ot the position class. current impl wont work************
                     #check if you need to enter position
-                    entry_price = C
-                    exit_price = C
+                    self.entry_price = C
+                    self.exit_price = C
 
-                    if (EMA1 > EMA2) and (highest == 2):
-                        highest = 1
-                        in_position = True
-                        upTrade = True
-                        entry_price = C
-                        exit_price = C - (C * stop_loss)
-                        print("EMA Cross: upTrade placed! Entry Price: " + str(entry_price))
-                        place_order(entry_price, 10)
+                    if (self.EMA1 > self.EMA2) and (self.highest == 2):
+                        self.highest = 1
+                        self.in_position = True
+                        self.upTrade = True
+                        self.entry_price = C
+                        self.exit_price = C - (C * self.stop_loss)
+                        print("EMA Cross: upTrade placed! Entry Price: " + str(self.entry_price))
+                        place_order(self.entry_price, 10)
 
-                    if (EMA1 < EMA2) and (highest == 1):
-                        highest = 2
-                        in_position = True
-                        upTrade = False
-                        entry_price = C
-                        exit_price = C + (C * self.stop_loss)
-                        print("EMA Cross: downTrade placed! Entry Price: " + str(entry_price))
-                        place_order(entry_price, -10)
+                    if (self.EMA1 < self.EMA2) and (self.highest == 1):
+                        self.highest = 2
+                        self.in_position = True
+                        self.upTrade = False
+                        self.entry_price = C
+                        self.exit_price = C + (C * self.stop_loss)
+                        print("EMA Cross: downTrade placed! Entry Price: " + str(self.entry_price))
+                        place_order(self.entry_price, -10)
 
 
             if(not self.Live_Trading):
-                Current_time = Current_time + timedelta(minutes=1)
+                self.Current_time = self.Current_time + timedelta(minutes=1)
 
             #print(Current_time)
             self.NewData = False
 
-        if (Current_time > self.DAY_END_TIME):
+        if (self.Current_time > self.DAY_END_TIME):
             print("DONE!")
-            print("# of trades: " + str(number_of_trades))
-            print("Good trades: " + str(good_trades) + " | Bad trades: " + str(bad_trades))
-            print("Total Profit: " + str(profit))
-            print("missing data: " + str(missing_data))
+            print("# of trades: " + str(self.number_of_trades))
+            print("Good trades: " + str(self.good_trades) + " | Bad trades: " + str(bad_trades))
+            print("Total Profit: " + str(self.profit))
+            print("missing data: " + str(self.missing_data))
             time.sleep(10)
             quit()
         #print("Notify Recieved")
         #UpdateDataArray(QueryData(ticker))
         #print(AM_candlesticks)
+
+
+    #Taken from Momentum_Test. Could be moved to algo mebe
+    def UpdateDataArray(self, data):
+    
+        if data != None:
+            change = 0.00
+            change = float(data[8])-float(data[6])
+            #print(change)
+
+            self.AM_candlesticks.append({
+                "dtime": data[0],
+                "open": data[6],
+                "high": data[7],
+                "low": data[9],
+                "close": data[8],
+                "volume": data[2],
+                "change": change
+            })
+
+            self.close_data.append(int(data[8]))
+
+            #print(len(AM_candlesticks))
+            if len(self.AM_candlesticks) > 20:
+                self.AM_candlesticks.pop(0)
+                self.close_data.pop(0)
+                #print(len(AM_candlesticks))
