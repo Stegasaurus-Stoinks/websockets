09233ddca80c8e4ce50ea7e841d9c85782763ad7
justinvred <justinvred@gmail.com>
2021-04-27 07:41:57 -0700

    scale filter changes

--- Discord-Scraper/config.py
+++ Discord-Scraper/config.py
@@ -14,11 +14,13 @@ eva-panda-trades ID: 821479719161561168
 
 
 #Server stuff
-GUILD_NAME = "Xtrades.net"
-CHANNEL_ID = 592829820371599451
+#GUILD_NAME = "Stoinks"
+#CHANNEL_ID = 821479719161561168
+
+GUILD_NAME = "Stoinks"
+CHANNEL_ID = 822972262008356964
 
 #Credential stuff
-    #Justin's account
 TOKEN_AUTH = "MTg5NjA0MTc4MTExMjM0MDUw.YEPoQQ.-kgEiLpyPGCzIeV58bxhPe_wQqo" #Retrieved from browser local storage.
 
 
