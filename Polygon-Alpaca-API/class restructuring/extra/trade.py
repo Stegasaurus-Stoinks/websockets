08643264df0bc26bbfb30d4c3f52f9a50cccde02
Jvredevoogd <jvredevoogd@cfms4.com>
2022-09-06 14:42:51 -0700

    small fix for trade recording

--- Polygon-Alpaca-API/class restructuring/extra/trade.py
+++ Polygon-Alpaca-API/class restructuring/extra/trade.py
@@ -98,7 +98,7 @@ class Trade:
         duration = self.closeTime - self.openTime
         
         if(display):
-            if self.backTest:
+            if not self.backTest:
                 f = open("live_tradey.txt", "a")
             else:
                 f = open("test_tradey.txt", "a")
